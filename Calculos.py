ff5281e93215b4af55d8b1cc145da9cf1346eae2
Angel Hincho <82984150+ahincho@users.noreply.github.com>
2022-10-02 23:05:58 -0500

    Archivo que implementa el Módulo de Operaciones

---
+++ Calculos.py
@@ -0,0 +1,17 @@
+
+# Autor: Hincho Jove, Angel
+# Archivo que implementa el modulo de Operaciones
+
+import Operaciones as Op
+
+def main():
+    a = 10.0
+    b = 'abc'
+    c = 3.0
+    d = 0.0
+    print(f"Suma: {Op.sumar(a, b)}")
+    print(f"Resta: {Op.restar(b, c)}")
+    print(f"Multiplicación: {Op.multiplicar(d, b)}")
+    print(f"División: {Op.dividir(c, d)}")
+
+main()
